a9bbe98f8cf9dec7f26d820bf94a9fa62a4a1518
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-05 17:38:25 +0300

    Update User.java

--- src/main/java/com/paintingscollectors/user/model/User.java
+++ src/main/java/com/paintingscollectors/user/model/User.java
@@ -1,10 +1,18 @@
 package com.paintingscollectors.user.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -20,7 +28,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    
+
 
 
 }
