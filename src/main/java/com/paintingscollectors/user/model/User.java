6e124ea43a86dd940b4c6f9ca8a8c94250e52da2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-05 17:33:44 +0300

    Add files via upload

---
+++ src/main/java/com/paintingscollectors/user/model/User.java
@@ -0,0 +1,26 @@
+package com.paintingscollectors.user.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    
+
+
+}
