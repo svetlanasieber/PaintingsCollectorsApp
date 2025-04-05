9ed3a644393ceb476558af011f4970eaeb1ae805
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-06 01:57:48 +0300

    Update Painting.java

--- PaintingsCollectors-App/src/main/java/com/paintingscollectors/painting/model/Painting.java
+++ PaintingsCollectors-App/src/main/java/com/paintingscollectors/painting/model/Painting.java
@@ -1,10 +1,13 @@
 package com.paintingscollectors.painting.model;
 
-import jakarta.persistence.Entity;
+import com.paintingscollectors.user.model.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
 @Setter
@@ -12,6 +15,33 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Painting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Style style;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+
+    private int votes;
+
+    @ManyToOne
+    private User owner;
+
+
+
+
 
 
 }
