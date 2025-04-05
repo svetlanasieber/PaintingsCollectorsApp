ed5e3751ec7a3f685a4e1922a3f5bc71cad5f2b0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-05 17:40:09 +0300

    Add files via upload

---
+++ PaintingsCollectors-App/src/main/java/com/paintingscollectors/painting/model/Painting.java
@@ -0,0 +1,17 @@
+package com.paintingscollectors.painting.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Painting {
+
+
+}
